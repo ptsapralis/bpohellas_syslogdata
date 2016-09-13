720cd39ae86084520c976c93740183c1d8c352c1
Panos Tsapralis <ptsapralis@gmail.com>
2016-09-13 15:45:50 +0300

    Update "slrectypeid" field in "bposyslogdb" database.
    After elimination of "slmsg" field in "syslogdata" table.

--- mssqlserver/update_mssql_dbo_bposyslogdb_syslogdata_slrectypeid_field_20160901.sql
+++ mssqlserver/update_mssql_dbo_bposyslogdb_syslogdata_slrectypeid_field_20160901.sql
@@ -9,3 +9,11 @@ where slb.slrec_message like '%login%exceed%limit%login%disabled%';
 update sld set slrectypeid=5
 from syslogdata sld join syslogbuffer slb on sld.slrec_id=slb.slrec_id
 where slb.slrec_message like '%reputation%db%maintenance%';
+
+update sld set slrectypeid=6
+from syslogdata sld join syslogbuffer slb on sld.slrec_id=slb.slrec_id
+where slb.slrec_message like '%type=traffic%srcport=%dstport=%';
+
+update sld set slrectypeid=7
+from syslogdata sld join syslogbuffer slb on sld.slrec_id=slb.slrec_id
+where slb.slrec_message like '%type=traffic%' and slrec_message not like '%srcport=%dstport=%';
